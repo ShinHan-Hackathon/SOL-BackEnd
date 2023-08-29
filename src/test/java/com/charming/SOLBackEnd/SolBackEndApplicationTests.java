d60c484369979d6f4eff8257b1d5dbe9a0a11714
Henry-Cha <sungwon4728@naver.com>
2023-08-29 22:41:21 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/charming/SOLBackEnd/SolBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charming.SOLBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
